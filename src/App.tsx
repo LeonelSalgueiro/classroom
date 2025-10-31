08c00d0724edcf76ecb433fb5bb64e5d95bae0b4
Leonel Salgueiro <leonel.ifcs@outlook.com>
2025-10-31 20:44:21 -0300

    input init

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import styles from "./app.module.css"
 import { Header } from "./components/Header"
 import { Tip } from "./components/Tip"
 import { Letter } from "./components/Letter"
+import { Input } from "./components/Input"
 
 
 export function App(){
@@ -18,7 +19,18 @@ export function App(){
       <main>
         <Header current={5} max={10} onRestart={handleRestartGame}/>
         <Tip tip="Linguagem de programação dinâmica."/>
-        <Letter value="" />
+        <div className={styles.word}>
+          <Letter value="R" />
+          <Letter value="E" />
+          <Letter value="A" />
+          <Letter value="C" />
+          <Letter value="T" />
+        </div>
+
+        <h4>Palpite</h4>
+        <div>
+          <Input autoFocus maxLength={1} placeholder="?" />
+        </div>
       </main>
     </div>
 
