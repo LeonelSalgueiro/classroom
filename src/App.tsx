9babfec5d09de77e3dfb6f26420719a8db43292a
Leonel Salgueiro <leonel.ifcs@outlook.com>
2025-10-31 20:01:37 -0300

    init tip

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import styles from "./app.module.css"
 
 import { Header } from "./components/Header"
+import { Tip } from "./components/Tip"
 
 
 export function App(){
+  function handleRestartGame(){
+    alert("Reiniciar o jogo!")
+  }
+
+
+
+
   return (
     <div className={styles.container}>
       <main>
-        <Header />
+        <Header current={5} max={10} onRestart={handleRestartGame}/>
+        <Tip tip="Linguagem de programação dinâmica."/>
       </main>
     </div>
 
