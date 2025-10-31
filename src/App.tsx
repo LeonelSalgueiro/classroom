1fe3d1355fa4836700ed475530331499f21cca11
Leonel Salgueiro <leonel.ifcs@outlook.com>
2025-10-31 20:14:09 -0300

    letter init

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import styles from "./app.module.css"
 
 import { Header } from "./components/Header"
 import { Tip } from "./components/Tip"
+import { Letter } from "./components/Letter"
 
 
 export function App(){
@@ -17,6 +18,7 @@ export function App(){
       <main>
         <Header current={5} max={10} onRestart={handleRestartGame}/>
         <Tip tip="Linguagem de programação dinâmica."/>
+        <Letter value="" />
       </main>
     </div>
 
