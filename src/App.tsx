940e5f1e3b19faec886ac5744b0c80877d95ded4
Leonel Salgueiro <leonel.ifcs@outlook.com>
2025-11-01 14:56:33 -0300

    button confirme

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Header } from "./components/Header"
 import { Tip } from "./components/Tip"
 import { Letter } from "./components/Letter"
 import { Input } from "./components/Input"
+import { Button } from "./components/Button"
 
 
 export function App(){
@@ -28,8 +29,9 @@ export function App(){
         </div>
 
         <h4>Palpite</h4>
-        <div>
+        <div className={styles.guess}>
           <Input autoFocus maxLength={1} placeholder="?" />
+          <Button title="Confirmar" />
         </div>
       </main>
     </div>
